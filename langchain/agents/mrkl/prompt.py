e31d8a706bc6e57439691a34ab018a75aa980dc5
mken33 <kmeiseki@gmail.com>
2023-06-13 11:08:56 +0900

    Update prompt.py

--- langchain/agents/mrkl/prompt.py
+++ langchain/agents/mrkl/prompt.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+'''
 PREFIX = """Answer the following questions as best you can. You have access to the following tools:"""
 FORMAT_INSTRUCTIONS = """Use the following format:
 
@@ -11,6 +12,20 @@ Observation: the result of the action
 Thought: I now know the final answer
 Final Answer: the final answer to the original input question"""
 SUFFIX = """Begin!
+'''
+
+PREFIX = """以下の質問に答えてください。あなたは次のツールを使うことができます:"""
+FORMAT_INSTRUCTIONS = """以下のフォーマットに従ってください:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question"""
+SUFFIX = """はじめてください!
 
 Question: {input}
 Thought:{agent_scratchpad}"""
