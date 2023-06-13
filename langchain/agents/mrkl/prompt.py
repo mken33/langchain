9f808e2bebf222ddc640c1d0e3e19adbaffeaa10
mken33 <kmeiseki@gmail.com>
2023-06-13 11:15:13 +0900

    Update prompt.py

--- langchain/agents/mrkl/prompt.py
+++ langchain/agents/mrkl/prompt.py
@@ -17,15 +17,15 @@ SUFFIX = """Begin!
 PREFIX = """以下の質問に答えてください。あなたは次のツールを使うことができます:"""
 FORMAT_INSTRUCTIONS = """以下のフォーマットに従ってください:
 
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question"""
+質問: あなたが答えることになる質問
+考え: 常に、あなたは何をすべきかを考えてください
+アクション: これから行うアクション。[{tool_names}]のいずれか一つであるべきです。
+アクションへの入力: アクションに対する入力
+観察: アクションに対する結果
+... (この、考え/アクション/アクションへの入力/観察は、N回、繰り返す可能性があります。)
+考え: 私は最終回答がわかりました。
+最終回答: 元の質問に対する最終的な答え"""
 SUFFIX = """はじめてください!
 
-Question: {input}
-Thought:{agent_scratchpad}"""
+質問: {input}
+考え:{agent_scratchpad}"""
